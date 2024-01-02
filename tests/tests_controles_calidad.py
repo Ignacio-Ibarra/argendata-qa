d0db489cb08714e7c6043ae9c6741d1dade866c7
joangq <jgquiroga@dc.uba.ar>
2024-01-02 15:25:59 -0300

    Agrego controles y boceto de tests

---
+++ tests/tests_controles_calidad.py
@@ -0,0 +1,19 @@
+# keys = ['key1', 'key2']
+# 
+# data_1 = pd.DataFrame({
+#     'key1': ['A', 'A', 'B', 'B'],
+#     'key2': ['X', 'X', 'X', 'X'],
+#     'other': [1, 2, 3, 4]
+# })
+# 
+# data_2 = pd.DataFrame({
+#     'key1': ['A', 'A', 'B', 'B'],
+#     'key2': ['X', 'X', 'Y', 'Y'],
+#     'other': [1, 2, 3, 4]
+# })
+# 
+# data_3 = pd.DataFrame({
+#     'key1': ['A', 'B', 'C', 'D'],
+#     'key2': ['X', 'Y', 'Z', 'W'],
+#     'other': [1, 2, 3, 4]
+# })
