6a754d7662f3a5910b143ba691308cb360660223
joangq <jgquiroga@dc.uba.ar>
2024-01-08 14:48:40 -0300

    Exporto resultados como JSON

--- argendata/__main__.py
+++ argendata/__main__.py
@@ -1,6 +1,10 @@
 import argendata.qa as qa
 from .utils.gwrappers import GAuth, GDrive
+from .utils import timeformat, parse_time_arg
+from datetime import datetime
 import pprint
+import json
+import pytz
 
 def main():
     auth = GAuth.authenticate()
@@ -9,6 +13,9 @@ def main():
     verificaciones = qa.analyze('TRANEN')
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(verificaciones)
+
+    with open('result-'+timeformat(datetime.now(tz=pytz.timezone('America/Argentina/Buenos_Aires')))+'.json', 'w') as fp:
+        json.dump(obj=verificaciones, indent=4, fp=fp)
 
 if __name__ == "__main__":
     main()
