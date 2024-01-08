00714861bd9b65d3c682e2e0554bb5d69eb6d34e
joangq <jgquiroga@dc.uba.ar>
2024-01-08 15:15:44 -0300

    Parametrizo main

--- argendata/__main__.py
+++ argendata/__main__.py
@@ -9,12 +9,14 @@ import pytz
 def main():
     auth = GAuth.authenticate()
     drive = GDrive(auth)
-
-    verificaciones = qa.analyze('TRANEN')
+    
+    subtopico = 'TRANEN'
+    verificaciones = qa.analyze(subtopico)
+    
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(verificaciones)
 
-    with open('result-'+timeformat(datetime.now(tz=pytz.timezone('America/Argentina/Buenos_Aires')))+'.json', 'w') as fp:
+    with open('result-'+subtopico+"-"+timeformat(datetime.now(tz=pytz.timezone('America/Argentina/Buenos_Aires')))+'.json', 'w') as fp:
         json.dump(obj=verificaciones, indent=4, fp=fp)
 
 if __name__ == "__main__":
