03a1d1b99b0d875cbb1ad0c007744599eab8f308
joangq <jgquiroga@dc.uba.ar>
2024-01-29 15:24:08 -0300

    Genero mapeos internos

--- argendata/freeze/__init__.py
+++ argendata/freeze/__init__.py
@@ -9,36 +9,28 @@ import csv
 
 logger = LoggerFactory.getLogger("freeze")
 
-def exportar_definitivo(nombre_subtopico: str, entrega: int, verificaciones_datasets: dict, ids = None):
+def exportar_definitivo(subtopico_obj, nombre_subtopico: str, entrega: int, verificaciones_datasets: dict, csv_map: dict, ids = None):
     logger.info(f'Exportando {nombre_subtopico}{str(entrega)} como definitivo...')
-    subtopico = Subtopico.from_name(nombre_subtopico, entrega)
+    #subtopico_obj = Subtopico.from_name(nombre_subtopico, entrega)
 
-    subtopico.dataset: GFolder
-    subtopico.plantilla: DataFrame
+    subtopico_obj.dataset: GFolder
+    subtopico_obj.plantilla: DataFrame
     file: GFile
 
     if ids is None:
-        ids = remapper.generate_ids(nombre_subtopico, subtopico.plantilla)
+        ids, _, _ = remapper.generate_ids(nombre_subtopico, subtopico_obj.plantilla)
 
-    csv_map = dict()
-    for k,v in ids['csv'].items():
-        csv_map.setdefault(v.strip(), []).append(k)
-    
-    mappings_file = \
-        makedirs(f'./output/{nombre_subtopico}{entrega}/definitivos/mappings.json')
+    # with open(mappings_file, 'w') as fp:
+    #     mappings = {k:list(map(ids["ID Gráfico"].__getitem__,v)) for k,v in csv_map.items()}
+    #     json.dump(mappings, indent=4, fp=fp)
 
-    with open(mappings_file, 'w') as fp:
-        mappings = {k:list(map(ids["ID Gráfico"].__getitem__,v)) for k,v in csv_map.items()}
-        json.dump(mappings, indent=4, fp=fp)
-
-    for file in subtopico.dataset.resources:
+    for file in subtopico_obj.dataset.resources:
         name = file.clean_title+file.extension
+        name = name.strip()
         path = file.download('./tmp/'+name)
-        
-        pr_ids = csv_map[name]
 
-        for pr_id in pr_ids:
-            pb_id = ids['ID Gráfico'][pr_id]
+        for mapping in csv_map[name]:
+            pr_id, pb_id = mapping['private'], mapping['public']
 
             logger.info(f"Mapping {name} -> {pr_id} <-> {pb_id}")
             
