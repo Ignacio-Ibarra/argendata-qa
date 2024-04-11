d638f9c1f999df62e231960b919654b002089654
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-04-11 12:14:39 -0300

    fix bad identation at line 2

--- argendata/qa/verificador/abstracto.py
+++ argendata/qa/verificador/abstracto.py
@@ -1,5 +1,5 @@
 from argendata.utils.logger import LoggerFactory
-    from typing import Protocol, TypeVar, Type, Generic, Final
+from typing import Protocol, TypeVar, Type, Generic, Final
 import inspect
 from inspect import Parameter
 from types import MappingProxyType
