8b445ddd05a5bcb047f9c41ee1fd76da0ad44f02
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-04-17 16:38:44 -0300

    cambié función jaccard_similarity, corregí algunos thresholds, agregué similaridad de jaccard basada en ngramas

--- argendata/utils/fuzzy_matching.py
+++ argendata/utils/fuzzy_matching.py
@@ -98,20 +98,42 @@ def normalize_string(s:str,
 def str_normalizer(normalize_params:dict)->Callable:
     return lambda s: normalize_string(s=s, **normalize_params)
 
-# Tokenización.. 
-def word_tokenize(normalized_string:str): 
-    return normalized_string.split(" ")
+# Tokenización..
+def ngrams_tokenizer(s: str, n:int=3) -> List[str]:
+    return [s[i:i+n] for i in range(len(s)-n+1)]
+ 
+def word_tokenizer(s:str)->list[str]: 
+    return s.split(" ")
+
+def character_tokenizer(s:str)->list[str]: 
+    return list(s)
 
 
 # Métricas
+def jaccard_similarity(s1:str, s2:str, tokenizer:Callable)->float:
+    set1 = set(tokenizer(s1))
+    set2 = set(tokenizer(s2))
+    intersect_n = len(set1 & set2)
+    union_n =len(set1 | set2)
+    return intersect_n / union_n
+
+def jaccard_words_similarity(s1:str,s2:str, tokenizer=word_tokenizer)->float:
+    return jaccard_similarity(s1,s2,tokenizer)
+
+def jaccard_character_similarity(s1:str,s2:str, tokenizer=character_tokenizer)->float:
+    return jaccard_similarity(s1,s2,tokenizer)
+
+def jaccard_ngrams_similarity(s1:str,s2:str, tokenizer=ngrams_tokenizer)->float:
+    return jaccard_similarity(s1,s2,tokenizer)
+
 def inclusion_en_derecha(s1,s2): 
-    set1 = set(word_tokenize(s1) )
-    set2 = set(word_tokenize(s2) ) 
+    set1 = set(word_tokenizer(s1) )
+    set2 = set(word_tokenizer(s2) ) 
     return int(set1 <= set2)
 
 def inclusion_en_izquierda(s1,s2): 
-    set1 = set(word_tokenize(s1) )
-    set2 = set(word_tokenize(s2) ) 
+    set1 = set(word_tokenizer(s1) )
+    set2 = set(word_tokenizer(s2) ) 
     return int(set1 >= set2)
         
 def levenshtein_distance(a, b):
@@ -137,19 +159,6 @@ def levenshtein_distance(a, b):
     # The bottom-right cell contains the minimum edit distance
     return matrix[n][m]
 
-def jaccard_words_similarity(s1:str,s2:str)->float:
-    set1 = set(word_tokenize(s1) )
-    set2 = set(word_tokenize(s2) )
-    intersect_n = len(set1 & set2)
-    union_n =len(set1 | set2)
-    return intersect_n / union_n
-
-def jaccard_character_similarity(s1:str,s2:str)->float:
-    set1 = set(s1)
-    set2 = set(s2)
-    intersect_n = len(set1 & set2)
-    union_n = len(set1 | set2)
-    return intersect_n / union_n
  
 def map_value(value, istart, istop, ostart, ostop):
     return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
@@ -168,15 +177,23 @@ def normalized_levenshtein_similarity(s1:str, s2:str, mapper:callable = my_mappe
 
 
 
-def smiliarity_scores(s1:str, s2:str)->bool:
+def similarity_scores(s1:str, s2:str)->bool:
     
     incl_der_scr = inclusion_en_derecha(s1,s2)
     incl_izq_scr = inclusion_en_izquierda(s1,s2)
     norm_lev_scr = normalized_levenshtein_similarity(s1,s2)
     jacc_w_scr = jaccard_words_similarity(s1,s2)
     jacc_c_scr = jaccard_character_similarity(s1, s2)
+    jacc_n_scr = jaccard_ngrams_similarity(s1,s2)
 
-    return [incl_der_scr, incl_izq_scr, norm_lev_scr, jacc_w_scr, jacc_c_scr]
+    return [
+        incl_der_scr, 
+        incl_izq_scr, 
+        norm_lev_scr, 
+        jacc_w_scr, 
+        jacc_c_scr, 
+        jacc_n_scr
+        ]
 
 def binarization_thresh(similarities_scores:list, threshs:list)->list[int]:
     return [scr >= th for scr,th in zip(similarities_scores, threshs)]
@@ -184,18 +201,24 @@ def binarization_thresh(similarities_scores:list, threshs:list)->list[int]:
 def combine_results(bin_results:list, weights:list):
     return np.dot(bin_results, weights)
 
-def evaluate_similarity(s1:str, s2:str, threshs:list=[1, 1, 0.4, 0.1, 0.1], weights:list = [0.2, 0.2, 0.2, 0.2, 0.2])->float:
+threshs_selected = [
+    1, 1, 
+    0.6, 0.1, 0.6, 0.6]
+n = len(threshs_selected)
+weights_selected = [1/n]*n
+
+def evaluate_similarity(s1:str, s2:str, threshs:list=threshs_selected, weights:list = weights_selected)->float:
     # if normalizer: 
     #     s1, s2 = list(map(normalizer, [s1,s2]))
-    sim_scr = smiliarity_scores(s1,s2)
+    sim_scr = similarity_scores(s1,s2)
     bin_results = list(map(int, binarization_thresh(similarities_scores=sim_scr, threshs=threshs)))
     result = combine_results(bin_results=bin_results, weights=weights)
     return result
 
-def likely_matching(s1:str, s2:str, threshs:list=[1, 1, 0.4, 0.1, 0.1], strictly=False, k:int=-1)->bool:
+def likely_matching(s1:str, s2:str, threshs:list=threshs_selected, strictly=False, k:int=-1)->bool:
     # if normalizer: 
     #     s1, s2 = list(map(normalizer, [s1,s2]))
-    sim_scr = smiliarity_scores(s1,s2)
+    sim_scr = similarity_scores(s1,s2)
     bin_results = binarization_thresh(similarities_scores=sim_scr, threshs=threshs)
     result = any(bin_results)
     if k > -1: 
